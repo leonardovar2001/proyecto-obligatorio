2c583eaf03b09482a05ff9d53fec9c6b13fee9eb
leonardovar2001 <leonardovargas2001@hotmail.com>
2022-08-18 16:52:35 -0300

    Productos Autos

---
+++ js/products.js
@@ -0,0 +1,49 @@
+let categoriesArray = [];
+
+//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.products.length; i++){ 
+        let car = array.products[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="`+ car.image +`" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ car.name +` - `+ car.cost +` `+ car.currency +`</h4> 
+                        <p> `+ car.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + car.soldCount  + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML=htmlContentToAppend;
+    }
+}
+
+/* 
+EJECUCIÓN:
+
+-Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
+-Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
+-Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
+
+*/
+
+document.addEventListener("DOMContentLoaded", function(){
+    getJSONData(PRODUCTS_URL2).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categoriesArray);
+        }
+    });
+});
